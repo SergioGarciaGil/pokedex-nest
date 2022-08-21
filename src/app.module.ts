2e7751605fca273740365783c4c5c2448c15026b
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-08-21 17:20:08 -0500

    crear-pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,9 @@ import { PokemonModule } from './pokemon/pokemon.module';
     }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
+
     PokemonModule
+
   ],
 
 })
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,19 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase()
+
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto)
+      return pokemon;
+
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon exist in db ${JSON.stringify(error.keyValue)} `)
+      }
+      console.log(error)
+      throw new InternalServerErrorException(`Ca't create Pokemon  - Check sever logs`)
+
+    }
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: term })
+    }
+    // MongoId para validar si es un objeto d mongo
+    if (isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term)
+    }
+    //Name para valiodar si encontramos en mongo por normalize
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() })
+    }
+
+    if (!pokemon) throw new NotFoundException(`Pokemon with id, name or no "${term}" not found`)
+    return pokemon
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
@@ -24,3 +60,7 @@ export class PokemonService {
     return `This action removes a #${id} pokemon`;
   }
 }
+function isValidObject(term: string) {
+  throw new Error('Function not implemented.');
+}
+
