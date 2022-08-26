b3e570b8003e455678fb12087ae732f8f5a530a0
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-08-26 09:54:05 -0500

    crear adaptador por si axios cambia

--- README.md
+++ README.md
@@ -23,8 +23,15 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Reconstruir la base de datos con la semilla
+
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack usados
 
 - MongoDB
 - Nest
+
 # pokedex-nest
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interfaces';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
+
 
 
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) { }
 
   async executeSeed() {
@@ -21,7 +23,7 @@ export class SeedService {
 
     await this.pokemonModel.deleteMany({})//delete *from pokemon
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
 
     const pokemonToInsert: { name: string, no: number }[] = []
 
