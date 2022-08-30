f8e700353d06817947844b416763d37fab9390af
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-08-30 10:08:13 -0500

    paginatio query

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { // para tansformar a strin las validaciones @query
+        enableImplicitConversion: true
+      }
+
     }),
   );
 
