774d5ca5881ca60d9c3fd1847b472a8878541cc6
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-09-06 23:02:55 -0500

    make it better

--- src/common/dto/pagination.Dto.ts
+++ src/common/dto/pagination.Dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsOptional, IsPositive, Min } from "class-validator"
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator'
 
 export class PaginationDto {
     @IsOptional()
